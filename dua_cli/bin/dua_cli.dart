f37944293fd8b572a873aba1808c7fe347b90fec
liuzhipeng <liuzhipeng@haxibiao.com>
2022-12-29 18:24:28 +0800

    cli

--- dua_cli/bin/dua_cli.dart
+++ dua_cli/bin/dua_cli.dart
@@ -10,22 +10,76 @@ void main(List<String> arguments) {
 
   var parser = ArgParser();
   parser.addCommand(
-    'init',
-    ArgParser()
-      ..addFlag('pageLocation', abbr: 'p')
-      ..addFlag('path', abbr: 'p'),
-  );
-  parser.addCommand(
     'create:page',
-    ArgParser()
-      ..addFlag('name', abbr: 'n')
-      ..addFlag('path', abbr: 'p'),
   );
-
   try {
     var results = parser.parse(arguments);
-    print("command: ${results.command}    arguments: ${results.arguments}");
+    var pageName = results.arguments[1];
+    bool useCupertino = results.arguments.indexWhere((element) => element.contains("-p")) != -1;
+
+    if (pageName.startsWith(RegExp(r'[a-z]'))) {
+      printerr("页面名应该大写字母开头、例如: UserDetail ");
+      return;
+    }
+    var cname = dua_cli.handleNameStr(pageName);
+
+    var libGenDua = join(pwd, "lib/pages/_dua");
+    var constantsGenFile = join(libGenDua, "constants.dart");
+    var pagesGenFile = join(libGenDua, "pages.dart");
+
+    var c1 = [];
+    List<String> c2 = [];
+    if (!exists(libGenDua)) {
+      createDir(libGenDua, recursive: true);
+    }
+    if (!exists(constantsGenFile)) {
+      touch(constantsGenFile, create: true);
+      constantsGenFile.write(dua_cli.tempConstants());
+    }
+    c1 = read(constantsGenFile).toList();
+    if (!exists(pagesGenFile)) {
+      touch(pagesGenFile, create: true);
+      pagesGenFile.write(dua_cli.tempPages());
+    }
+    c2 = read(pagesGenFile).toList();
+
+    // PAGE_USER_DETAIL
+    String PAGE_NAME = "PAGE_${cname.uppercase}";
+
+    // 第一步、写文件
+    var pageDir = join(pwd, "lib/pages/${cname.lowercase}");
+    if (!exists(pageDir)) {
+      createDir(pageDir, recursive: true);
+    }
+    var pageFile = join(pageDir, "${cname.lowercase}_page.dart");
+    var storeFile = join(pageDir, "${cname.lowercase}_store.dart");
+    if (!exists(pageFile)) {
+      touch(pageFile, create: true);
+    }
+    if (!exists(storeFile)) {
+      touch(storeFile, create: true);
+    }
+    pageFile.write(dua_cli.page(cname.origin, cname.lowercase, PAGE_NAME));
+    storeFile.write(dua_cli.store(cname.origin));
+
+    // 第二步、写PAGE常量
+    var newContent = "const $PAGE_NAME = \"${cname.origin}\";";
+    c1.add(newContent);
+    constantsGenFile.write(c1.join('\n'));
+
+    // 第三步、写路由
+    String pagetype = useCupertino ? "buildCupertinoStackNavigationPage" : "buildMaterialStackNavigationPage";
+    newContent = "\t$pagetype($PAGE_NAME, const ${cname.origin}Page()),";
+
+    var ind = c2.lastIndexWhere((String element) => element.contains("Auto-Generate"));
+    c2.insert(ind + 1, newContent);
+    c2.insert(ind + 2, "");
+    c2.insert(ind + 3, "\t///Auto-Generate");
+    c2.insert(1, "import '../${cname.lowercase}/${cname.lowercase}_page.dart';");
+    pagesGenFile.write(c2.join('\n'));
+
+    print('🎉done');
   } catch (e) {
-    print("命令使用出错");
+    dua_cli.printHelp();
   }
 }
--- dua_cli/lib/dua_cli.dart
+++ dua_cli/lib/dua_cli.dart
@@ -2,24 +2,207 @@ import 'package:dcli/dcli.dart';
 
 void printHelp() {
   print('''
-Accelerate your app development with dua cli.
-
-Common commands:
-  dua init
-
-Usage: dua <command> [arguments]
-
-Available commands:
-
-  init              Config project,install libs and set default configs
-  create            Create template file like page,store etc...
-    1. create page with page-scoped statemanager
-    dua create:page --name <page name> --path <page path> --store <store name default to page name with Store suffix>
-
-    2. create simple statemanager template
-    dua create:state --name
-
-Run "dua help <command>" for more information about a command.
-Run "dua help -v" for verbose help output, including less commonly used options.
+用法说明:
+  创建页面
+  > create:page UserDetail
+  创建基于CupertinoPage的页面
+  > create:page UserDetail -p
 ''');
+}
+
+// 用法: dua <command> [arguments]
+// Run "dua help <command>" for more information about a command.
+// Run "dua help -v" for verbose help output, including less commonly used options.
+String tempConstants() {
+  return '''
+///Auto-Generate@Constants
+''';
+}
+
+String tempPages() {
+  return '''
+///Auto-Generate@Pages
+
+///
+import './constants.dart';
+import 'package:dua/navigation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///Auto-Generate:buildMaterialStackNavigationPage
+DuaStackNavigationPage buildMaterialStackNavigationPage(String name, Widget page) {
+  return DuaStackNavigationPage(name, MaterialPage(name: name, child: page));
+}
+
+///Auto-Generate:buildCupertinoStackNavigationPage
+DuaStackNavigationPage buildCupertinoStackNavigationPage(String name, Widget page) {
+  return DuaStackNavigationPage(name, CupertinoPage(name: name, child: page));
+}
+
+///Auto-Generate:NavigationPages
+List<DuaStackNavigationPage> NavigationPages = <DuaStackNavigationPage>[
+  ///Auto-Generate
+];
+''';
+}
+
+String page(String name, String lowername, String pageconstants) {
+  return '''
+import 'package:dua/navigation.dart';
+import 'package:dua/widgets.dart';
+import 'package:dua/structure.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../_dua/constants.dart';
+import './${lowername}_store.dart';
+
+class ${name}Page extends StatefulWidget {
+  const ${name}Page({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _${name}Page();
+}
+
+class _${name}Page extends State<${name}Page> with DuaNavigationFocusMixin, DuaNavigationAutoReleaseMixin, DuaNavigationParamResultMixin, DuaNavigationBackHandlerMixin {
+  @override
+  String get name => $pageconstants;
+
+  @override
+  List makeAutoReleaseResource() {
+    return [${name}Store()];
+  }
+
+  @override
+  Future<bool> onBackHandlerInvoked() async {
+    /// return true to prevent default action;
+    return false;
+  }
+
+  @override
+  void onFocusChanged(bool focused) {
+    if (focused) {
+      /// 如果有页面返回结果 @get-property routeResult
+      /// debugPrint("\$routeResult");
+    }
+  }
+
+  @override
+  void initState() {
+    loadNavigationFocus();
+    loadNavigationBackHandler();
+    loadNavigationAutoRelease();
+    super.initState();
+
+    /// 如果有页面跳转参数 @get-property routeParams
+    /// debugPrint("\$routeParams");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    disposeNavigationFocus();
+    disposeNavigationBackHandler();
+    disposeNavigationAutoRelease();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PlainNavigationBar(
+              title: $pageconstants,
+              onPressBack: () {
+                context.goBack();
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(38.0),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("Page $name", style: TextStyle(color: Colors.blue, fontSize: 18)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+''';
+}
+
+String store(String name) {
+  return '''
+import 'package:dua/structure.dart';
+
+class ${name}Store with Observable {
+  ${name}Store() {
+    makeAutoObservable();
+  }
+
+  late var count = 666.ov(i);
+
+  late var text = "hello world - $name".ov(i);
+
+  setText(String v) {
+    text.value = v;
+    update();
+  }
+
+  setCount(int v) {
+    count.value = v;
+    update();
+  }
+}
+''';
+}
+
+class ConvertedName {
+  String origin;
+  List<String> segs;
+  String uppercase;
+  String lowercase;
+
+  ConvertedName(this.origin, this.segs, this.uppercase, this.lowercase);
+}
+
+ConvertedName handleNameStr(String name) {
+  var uppers = [];
+  for (var i = 0; i < name.length; i++) {
+    var unit = name.codeUnitAt(i);
+    if (unit >= 65 && unit < 97) {
+      uppers.add(i);
+      continue;
+    }
+  }
+  // UserProfileSetting
+  var segs = <String>[];
+  for (var i = 0; i < uppers.length; i++) {
+    var start = uppers[i];
+    var end = -1;
+    if ((i + 1) >= uppers.length) {
+      end = name.length;
+    } else {
+      end = uppers[i + 1];
+    }
+    segs.add(name.substring(start, end));
+  }
+  var upsegs = <String>[];
+  var lwsegs = <String>[];
+  for (var i = 0; i < segs.length; i++) {
+    upsegs.add(segs[i].toUpperCase());
+  }
+  for (var i = 0; i < segs.length; i++) {
+    lwsegs.add(segs[i].toLowerCase());
+  }
+  return ConvertedName(
+    name,
+    segs,
+    upsegs.join('_'),
+    lwsegs.join('_'),
+  );
 }
